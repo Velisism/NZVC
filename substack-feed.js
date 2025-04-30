7c7ac18a3577aa7e8e92398fee8f70b25f65ae62
Velisism <nataly@learnwithmochi.com>
2025-04-30 22:46:54 +0300

    Update substack-feed.js

--- substack-feed.js
+++ substack-feed.js
@@ -1,6 +1,6 @@
 /* substack-feed.js */
 const FEED_URL =
-  "https://api.rss2json.com/v1/api.json?rss_url=https://nzvc.substack.com/feed&api_key=YOUR_KEY";
+  "https://api.rss2json.com/v1/api.json?rss_url=https://nzvc.substack.com/feed&api_key=wchntgtgmpfmftvuinc2q3daw3lczgu1ytqimofx";
 const POSTS_PER_PAGE = 5;
 let posts = [], page = 1;
 
